7916f72a7deff29bd01c389c5ad6f45c8a5590a1
Ziyad Bhombal <zeebhombal@gmail.com>
2022-08-28 22:43:44 +0530

    Fixed type error

--- src/util/getCity.ts
+++ src/util/getCity.ts
@@ -1,13 +1,15 @@
-import { City } from './interfaces';
+import { City } from "./interfaces";
 import cities from "../lib/city.list.json";
 
-export default function getCity(slug:string){
+export default function getCity(slug: string) {
   const cityParams = slug.trim().split("-");
 
   const cityID = cityParams.pop();
-  if(!cityID) return null;
+  if (!cityID) return null;
 
-  const city = (cities as City[]).find(city => city.id.toString() == cityID)
+  const city = (cities as unknown as City[]).find(
+    (city) => city.id.toString() == cityID
+  );
 
   return city ? city : null;
 }
